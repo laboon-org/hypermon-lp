0fd0db0b7929a638e77fb425f55167796a46f364
nguyen84254 <nguyen84254@st.vimaru.edu.vn>
2021-12-28 11:25:57 +0700

    firts

---
+++ components/Login.js
@@ -0,0 +1,228 @@
+export default function Login () {
+  return (
+    <A_0_Signup_Viaemail>
+      <ConvertedImage />
+      <Bulbasaur1Image />
+      <Squitle31Image />
+      <logo1Image />
+      <Group54>
+        <Rectangle2 />
+      </Group54>
+      <Normal />
+      <Caterpie1a1Image />
+      <Charmander21Image />
+      <pikapika31Image />
+      <Group7727>
+        <IconImage />
+        <EmailText>Email</EmailText>
+      </Group7727>
+      <Group7726>
+        <IconImage />
+        <PasswordText>Password</PasswordText>
+      </Group7726>
+      <Line1 />
+      <SIGNUPText>SIGN UP</SIGNUPText>
+      <SIGNINText>SIGN IN</SIGNINText>
+      <TextField>
+        <Line2 />
+        <EnteryouremailText>Enter your password</EnteryouremailText>
+      </TextField>
+      <Icon>
+        <VectorImage />
+        <VectorImage />
+        <Line15 />
+      </Icon>
+      <TextField>
+        <Line2 />
+        <EnteryouremailText>Enter your email</EnteryouremailText>
+      </TextField>
+      <Group7725>
+        <Icon>
+          <Rectangle9 />
+        </Icon>
+        <IagreetothetermsandpoliciesText>
+          I agree to the terms and policies
+        </IagreetothetermsandpoliciesText>
+      </Group7725>
+      <Button>
+        <Rectangle8 />
+        <SignUpText>Sign Up</SignUpText>
+      </Button>
+      <IconImage />
+    </A_0_Signup_Viaemail>
+  );
+};
+
+const A_0_Signup_Viaemail = styled.View`
+  height: 1024px;
+  width: 1440px;
+  background-color: #ffffff;
+`;
+const ConvertedImage = styled.View`
+  height: 1024px;
+  width: 2305px;
+`;
+const Bulbasaur1Image = styled.View`
+  height: 281px;
+  width: 374px;
+`;
+const Squitle31Image = styled.View`
+  height: 244px;
+  width: 217px;
+`;
+const logo1Image = styled.View`
+  height: 111px;
+  width: 380px;
+`;
+const Group54 = styled.View`
+  opacity: 0.699999988079071;
+  height: 1024px;
+  width: 690px;
+`;
+const Rectangle2 = styled.View`
+  height: 1024px;
+  width: 690px;
+  background-color: #000b24;
+`;
+const Normal = styled.View`
+  height: 24px;
+  width: 44px;
+  background-color: #ffffff;
+`;
+const Caterpie1a1Image = styled.View`
+  transform: rotate(-180deg);
+  height: 97px;
+  width: 130px;
+`;
+const Charmander21Image = styled.View`
+  transform: rotate(-180deg);
+  height: 285px;
+  width: 290px;
+`;
+const pikapika31Image = styled.View`
+  height: 503px;
+  width: 403px;
+`;
+const Group7727 = styled.View`
+  height: 30px;
+  width: 96px;
+`;
+const IconImage = styled.View`
+  height: 30px;
+  width: 30px;
+`;
+const EmailText = styled.Text`
+  text-align: left;
+  vertical-align: top;
+  font-family: Alata;
+  font-weight: regular;
+  font-size: 20px;
+  line-height: auto;
+  color: #ffffff;
+`;
+const Group7726 = styled.View`
+  height: 30px;
+  width: 131px;
+`;
+const PasswordText = styled.Text`
+  text-align: left;
+  vertical-align: top;
+  font-family: Alata;
+  font-weight: regular;
+  font-size: 20px;
+  line-height: auto;
+  color: #ffffff;
+`;
+const Line1 = styled.View`
+  height: 0px;
+  width: 32px;
+  border: 5px solid #ffffff;
+`;
+const SIGNUPText = styled.Text`
+  text-align: left;
+  vertical-align: top;
+  font-family: Alata;
+  font-weight: regular;
+  font-size: 30px;
+  line-height: auto;
+  color: #ffffff;
+`;
+const SIGNINText = styled.Text`
+  text-align: left;
+  vertical-align: top;
+  font-family: Alata;
+  font-weight: regular;
+  font-size: 30px;
+  line-height: auto;
+  color: #ffffff;
+`;
+const TextField = styled.View`
+  height: 43px;
+  width: 549px;
+`;
+const Line2 = styled.View`
+  transform: rotate(-1deg);
+  height: 0px;
+  width: 549px;
+  border: 0.5px solid #777777;
+`;
+const EnteryouremailText = styled.Text`
+  text-align: left;
+  vertical-align: top;
+  font-family: Alata;
+  font-weight: regular;
+  font-size: 18px;
+  line-height: auto;
+  color: #777777;
+`;
+const Icon = styled.View`
+  height: 30px;
+  width: 30px;
+  background-color: #ffffff;
+`;
+const Line15 = styled.View`
+  transform: rotate(-140deg);
+  height: 0px;
+  width: 31px;
+  border: 2px solid #ffffff;
+`;
+const Group7725 = styled.View`
+  height: 30px;
+  width: 308px;
+`;
+const Rectangle9 = styled.View`
+  border-radius: 8px;
+  height: 30px;
+  width: 30px;
+  border: 2px solid #ffffff;
+`;
+const IagreetothetermsandpoliciesText = styled.Text`
+  text-align: left;
+  vertical-align: top;
+  font-family: Alata;
+  font-weight: regular;
+  font-size: 18px;
+  line-height: auto;
+  color: #ffffff;
+`;
+const Button = styled.View`
+  height: 60px;
+  width: 277px;
+  background-color: #ffffff;
+`;
+const Rectangle8 = styled.View`
+  border-radius: 10px;
+  height: 60px;
+  width: 277px;
+  background-color: ;
+  border: 2px solid #4cacec;
+`;
+const SignUpText = styled.Text`
+  text-align: left;
+  vertical-align: top;
+  font-family: Alata;
+  font-weight: regular;
+  font-size: 20px;
+  line-height: auto;
+  color: #ffffff;
+`;
