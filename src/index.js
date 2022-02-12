ff56c690876589958f01eec021c0f0c328074bc2
Steve Le <>
2022-02-12 15:19:51 +0700

    feat: add react bootstrap lib

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
 import "./index.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
