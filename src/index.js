acf04a6c84a7fdfa8884370ed99d2509e4036824
Steve Le <>
2022-02-13 16:02:53 +0700

    feat: re-enabled tailwind module

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "./index.scss";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+// tailwind.config.js
 module.exports = {
-  mode: "jit", // Add Just In Time Mode
-  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
