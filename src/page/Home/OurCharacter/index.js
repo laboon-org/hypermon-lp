92b09ea20002411398718ae43ca2e6eb30d9f68e
Nguyen Duc Tu <87971083+Hyena82@users.noreply.github.com>
2022-02-14 11:51:45 +0700

    refactoring code to responsive

--- src/page/Home/OurCharacter/index.js
+++ src/page/Home/OurCharacter/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./index.scss";
 import textOur from "./img/ourcharacterText.png";
 import bg from "./img/character11.png";
+import background from "./img/ourcharacter_background.png";
 import item1 from "./img/charmande/item1.png";
 import item2 from "./img/charmande/item2.png";
 import item3 from "./img/charmande/item3.png";
@@ -10,23 +11,24 @@ import star from "./img/charmande/star.png";
 import Charmande1 from "./img/Charmande1.png";
 import Vector from "./img/Vector.png";
 import Fire from "./img/fire.png";
+import "./mobile.scss"
 const OurCharacter = () => {
   return (
     <div style={{ backgroundColor: "#051435" }}>
       <div className="Ourcharacter mx-auto">
         <div className="mx-auto pt-28">
-          <img className="mx-auto" src={textOur} alt="" />
+          <img className="mx-auto mb-name-Ourcharacter" src={textOur} alt="" />
         </div>
         <div
           style={{
-            backgroundImage: `url(${bg})`,
+            backgroundImage: `url(${background})`,
             backgroundRepeat: "no-repeat",
             backgroundSize: "contain",
-            height: 1200,
           }}
           className="container-full mx-auto"
         >
-          <div className="container mx-auto">
+          <div className=" mx-auto" style={{ position: "relative" }}>
+            <p className="name-character">CHARMANDE</p>
             <div className="Ourcharacter__item">
               <div className="flex justify-center items-center">
                 <div>
@@ -35,8 +37,8 @@ const OurCharacter = () => {
                 <div>
                   <img src={item2} alt="" />
                 </div>
-                <div>
-                  <img className="px-20" src={star} alt="" />
+                <div className="wrapper-star">
+                  <img className="start-img" src={star} alt="" />
                 </div>
                 <div>
                   <img src={item3} alt="" />
@@ -46,31 +48,45 @@ const OurCharacter = () => {
                 </div>
               </div>
             </div>
-            <div>
-              <div>
-                <div className="Charmande1 relative">
-                  <img
-                    className="mx-auto vector absolute  "
-                    src={Vector}
-                    alt=""
-                  />
-                  <img
-                    className="mx-auto Charmande__img relative"
-                    src={Charmande1}
-                    alt=""
-                  />
-                  <img
-                     className="mx-auto fire__img"
-                     src={Fire}
-                     alt=""
-                  />
-                </div>
+
+            <div className="Charmande1 relative">
+              <img
+                className="mx-auto vector absolute  "
+                src={Vector}
+                alt=""
+              />
+              <img
+                className="mx-auto Charmande__img relative"
+                src={Charmande1}
+                alt=""
+              />
+              <img
+                className="fire__img"
+                src={Fire}
+                alt=""
+              />
+              <div className="character-properties">
+                <li><span> &#183;</span>&nbsp;&nbsp;&nbsp;SPIT FILE</li>
+                <li><span> &#183;</span>&nbsp;&nbsp;&nbsp;THUNDER</li>
+                <li><span> &#183;</span>&nbsp;&nbsp;&nbsp;DISCHARG</li>
               </div>
+            </div>
+            <div className="Ourcharacter_description">
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip
+              </span>
+              <span>
+                2.630 HPM
+              </span>
             </div>
           </div>
         </div>
       </div>
     </div>
+
   );
 };
 
