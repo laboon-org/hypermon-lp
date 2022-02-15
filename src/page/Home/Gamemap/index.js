137676ca7d3fac744f55ae9549278b15c6bbd6ab
Nguyen Duc Tu <87971083+Hyena82@users.noreply.github.com>
2022-02-15 10:18:38 +0700

    add classname

--- src/page/Home/Gamemap/index.js
+++ src/page/Home/Gamemap/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import "./index.scss";
 import GameMap from "./img/gamemap.png";
 import Map from "./img/map1.png";
+import "./mobile.scss"
 const index = () => {
   return (
     <div className="Gamemap mx-auto" style={{ backgroundColor: "#051435" }}>
       <div className="container mx-auto pt-20">
         <div>
-          <img className="mx-auto" src={GameMap} alt="" />
+          <img className="mx-auto mb-header-gamemap" src={GameMap} alt="" />
         </div>
         <div>
           <div className="Gamemap__content mt-10">
-            <p className="mx-auto">
+            <p className="mx-auto mb-description-gameplay">
               At vero eos et accusamus et iusto odio dignissimos ducimus qui
               blanditiis praesentium voluptatum deleniti atque corrupti quos
               dolores et quas molestias excepturi sint occaecati cupiditate non
