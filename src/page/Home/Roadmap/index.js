83e3665cbb89e31ec3b3214621319cbc02635da4
Nguyen Duc Tu <87971083+Hyena82@users.noreply.github.com>
2022-02-16 09:41:08 +0700

    refactoring code and add class

--- src/page/Home/Roadmap/index.js
+++ src/page/Home/Roadmap/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./index.scss";
 import roadmap from "./img/roadmap.png";
 import bg from "./img/bg.png";
+import road_responsive from "./img/road_develop.png";
 import Road from "./img/Road.png";
-
+import "./tablet.scss"
+import "./mobile.scss"
 const index = () => {
   return (
     <div
@@ -15,9 +17,9 @@ const index = () => {
         backgroundSize: "cover",
       }}
     >
-      <div className="container mx-auto " style={{ paddingTop: 200 }}>
+      <div className="mx-auto main-wrapper">
         <div>
-          <img className="mx-auto" src={roadmap} alt="" />
+          <img className="mx-auto header-name" src={roadmap} alt="" />
         </div>
         <div className="Roadmap__content mx-auto mt-10">
           <p>
@@ -26,9 +28,58 @@ const index = () => {
             voluptas assumenda est, omnis dolor repellendus.
           </p>
         </div>
-        <div style={{ paddingTop: 400, marginBottom: -205 }}>
+        <div className="maincontainer" >
           <div>
-            <img src={Road} alt="" />
+            <div className="wrapper-text">
+              <div>
+                <span>1/2022</span>
+                <span> <span className="symbol-text">I&nbsp;</span> Step one</span>
+                <span>
+                Nam libero tempore, cum soluta nobis est eligendi
+                  optio cumque nihil impedit
+                  quo minus id quod maxime placeat facere.
+                </span>
+              </div>
+              <div>
+                <span>2/2022</span>
+                <span><span className="symbol-text">I&nbsp;</span>Step second</span>
+                <span>
+                  Nam libero tempore, cum soluta nobis est eligendi
+                  optio cumque nihil impedit
+                  quo minus id quod maxime placeat facere.
+                </span>
+              </div>
+              <div>
+                <span>3/2022</span>
+                <span><span className="symbol-text">I&nbsp;</span>Step third</span>
+                <span>
+                  Nam libero tempore, cum soluta nobis est eligendi
+                  optio cumque nihil impedit
+                  quo minus id quod maxime placeat facere.
+                </span>
+              </div>
+              <div>
+                <span>4/2022</span>
+                <span><span className="symbol-text">I&nbsp;</span>Step four</span>
+                <span>
+                  Nam libero tempore, cum soluta nobis est eligendi
+                  optio cumque nihil impedit
+                  quo minus id quod maxime placeat facere.
+                </span>
+              </div>
+              <div>
+                <span>5/2022</span>
+                <span><span className="symbol-text">I&nbsp;</span>Step five</span>
+                <span>
+                  Nam libero tempore, cum soluta nobis est eligendi
+                  optio cumque nihil impedit
+                  quo minus id quod maxime placeat facere.
+                </span>
+              </div>
+            </div>
+
+            <img className="imgroad mx-auto" src={Road} alt="" />
+            <img className="imgroad-responsive" src={road_responsive} alt="" />
           </div>
         </div>
       </div>
