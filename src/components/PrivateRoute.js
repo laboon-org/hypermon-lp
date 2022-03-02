a3d7c34e309eb86daa4341b475a8b8edd8dee8c6
Duc Tu <87971083+Hyena82@users.noreply.github.com>
2022-03-02 21:17:04 +0700

    Apply: Private router and react-router-dom-v6.2.1

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import HomePage from '../page/Home';
+import ErrorBoundary from '../util/ErrorBoundary';
+export const PrivateRoute = () => {
+  return (
+    localStorage.getItem('auth')
+      ? <ErrorBoundary>
+        <HomePage />
+        </ErrorBoundary>
+      : <Navigate to="/login" />
+  )
+}
