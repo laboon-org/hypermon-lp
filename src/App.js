ddf2e33209976fb03e266582ed9911e2fcb3153f
Phoebe Vu <phoebevu68@gmail.com>
2022-04-23 14:13:04 +0700

    feat: add Storyblok.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { PrivateRoute } from './components/PrivateRoute';
 import LoginPage from './LoginPage/LoginPage';
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  concat,
+  HttpLink,
+} from "@apollo/client";
 import App from "./App";
-
 import "./styles/index.scss";
 
+const httpLink = new HttpLink({ uri: "https://gapi.storyblok.com/v1/api" });
+ 
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      token: process.env.REACT_APP_PREVIEW_TOKEN,
+      version: "draft",
+    },
+  }));
+  return forward(operation);
+});
+ 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
+});
+console.log("client check----", client);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
